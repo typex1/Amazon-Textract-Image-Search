d58b2b7c4f4f86f186ca0643bcbb894cf1441f2d
FS <fspiess@147ddaa45603.ant.amazon.com>
2022-04-06 16:27:20 +0200

    image folder update

--- retrieve_images.py
+++ retrieve_images.py
@@ -14,8 +14,8 @@ import platform
 
 debug = False
 ##### set IMAGE_DIR to match your image folder ##########
-IMAGE_DIR=os.environ['HOME']+'/Desktop/Screenshots'
-#IMAGE_DIR='./images'
+#IMAGE_DIR=os.environ['HOME']+'/Desktop/Screenshots'
+IMAGE_DIR='./images'
 TEXTRACT_OUTPUT_DIR = IMAGE_DIR + "/textract-data"
 
 txtNames = []
